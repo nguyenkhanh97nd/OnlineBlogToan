6026f273c2740f8637745503f93d93df9d256a47
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-04-24 23:37:54 +0700

    backend

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -75,36 +75,31 @@ class SettingController extends Controller
 
             $username = $user->username;
             $name = str_random(4)."_".$username.'.'.$extension;
-            while(file_exists("upload/users/".$name)){
+            while(file_exists("public/upload/users/".$name)){
                 $name = str_random(4)."_".$username.'.'.$extension;
             }
-            if(file_exists('public/upload/users/test.txt')) {
-                $sao = 'Co';
-            } else {
-                $sao = 'Khong';
+        
+            $path = 'public/upload/users/'.$name;
+
+            file_put_contents($path, $decoded);
+
+            if(filesize($path) > 500000) {
+                unlink($path);
+                return response()->json([
+                    'wrong_avatar' => 'Ảnh phải nhỏ hơn 500KB'
+                ]);
             }
-            // $path = 'upload/users/'.$name;
-
-            // file_put_contents($path, $decoded);
-
-            // if(filesize($path) > 500000) {
-            //     unlink($path);
-            //     return response()->json([
-            //         'wrong_avatar' => 'Ảnh phải nhỏ hơn 500KB'
-            //     ]);
-            // }
 
             // if($user->avatar && file_exists('upload/users/'.$user->avatar)) {
             //     unlink('upload/users/'.$user->avatar);
             // }
 
-            // $user->avatar = $name;
+            $user->avatar = $name;
         }
         $user->save();
 
         return response()->json([
-            // 'success' => 'Cập nhật ảnh thành công',
-            'success' => $sao
+            'success' => 'Cập nhật ảnh thành công',
         ]);
     }
 
