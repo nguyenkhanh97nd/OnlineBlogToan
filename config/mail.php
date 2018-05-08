21c7c84a00ae67a63b6147a2b4476d4444d470ac
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-05-08 20:42:07 +0700

    fix forgot

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'blogtoan.com@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Blog Toán'),
     ],
 
