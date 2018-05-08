07583d5ece549f3814b2d831e184319436b77a0a
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-05-08 20:32:48 +0700

    fix forgot

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -30,31 +30,4 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
-
-    public function sendResetLinkEmail(Request $request) {
-        // $this->validateEmail($request);
-
-        // $data = array();
-        // $data['email'] = $request->email;
-
-        // Mail::send('pages.mail.verify', $data, function($message) use ($data){
-        //     $message->from('blogtoan.com@gmail.com', 'Blog Toán');
-        //     $message->subject('Quên mật khẩu');
-        //     $message->to($data['email']);
-        // });
-
-        $this->validateEmail($request);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        
-        $response = $this->broker()->sendResetLink(
-            $request->only('email')
-        );
-
-        return $response == Password::RESET_LINK_SENT
-                    ? $this->sendResetLinkResponse($response)
-                    : $this->sendResetLinkFailedResponse($request, $response);
-    }
 }
