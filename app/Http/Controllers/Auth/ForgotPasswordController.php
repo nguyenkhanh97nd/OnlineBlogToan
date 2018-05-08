fce736e9d77693ba70590bf04cbba360b697c906
Khanh Nguyen <khanhnguyen@MacBook.local>
2018-05-08 19:46:42 +0700

    fix forgot

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +29,18 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function sendResetLinkEmail(Request $request) {
+        $this->validateEmail($request);
+
+        $data = array();
+        $data['email'] = $request->email;
+
+        Mail::send('pages.mail.verify', $data, function($message) use ($data){
+            $message->from('blogtoan.com@gmail.com', 'Blog Toán');
+            $message->subject('Quên mật khẩu');
+            $message->to($data['email']);
+        });
     }
 }
